ffb0ca3f6d21ecaa64082667f19f8e1b1fb086b1
Satche <info@thomas-robert.ch>
2025-03-17 14:12:52 +0100

    feat: add first content

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import '~/assets/css/index.css'
+
+const routes = [
+    { title: "Personnages", path: "/characters"},
+]
+</script>
+
+<template>
+    <h1>Welcome</h1>
+    <Collection :data="routes" />
+</template>
