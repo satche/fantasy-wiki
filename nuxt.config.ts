e9c9f096b011db34221fda51ea012cf4ee0877c0
Satche <info@thomas-robert.ch>
2025-05-06 16:15:21 +0200

    test: transition and layouts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   app: {
     head: {
       title: "Asema",
--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -1,15 +1,10 @@
-<script setup lang="ts">
-const route = useRoute()
-const { data: page } = await useAsyncData(route.path, () => {
-    const collection = queryCollection('characters').path(route.path).first()
-    return collection
-})
+<script setup>
 </script>
 
 <template>
-    <div>
-        <ContentRenderer v-if="page"
-                         :value="page" />
-
-    </div>
+  <div>
+    <NuxtLayout name="detail">
+      <slot />
+    </NuxtLayout>
+  </div>
 </template>
