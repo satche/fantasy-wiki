e057794b4cc8926d6979dab0c36049c93a326601
Satche <info@thomas-robert.ch>
2025-05-12 11:38:48 +0200

    feat: add lore pages

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nav = [
+  { "title": "Home", "path": "/" },
+  { "title": "Personnages", "path": "/characters" },
+  { "title": "Lore", "path": "/lore" }
+]
+</script>
 
 <template>
   <header>
     <nav>
       <ul>
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/characters">Personnages</NuxtLink>
+        <li v-for="page in nav">
+          <NuxtLink :to="page.path"
+                    :class="page.path == '/' || { 'parent-active-class': $route.path.startsWith(page.path) }">
+            {{ page.title }}
+          </NuxtLink>
         </li>
       </ul>
     </nav>
@@ -33,5 +39,10 @@ a {
   padding: 0.5rem 1rem;
   border: 1px solid var(--color-text);
   text-decoration: none;
+}
+
+.router-link-active,
+.parent-active-class {
+  color: var(--color-link-active)
 }
 </style>
--- layouts/collection.vue
+++ layouts/collection.vue
@@ -1,11 +1,11 @@
-<script setup>
+<script setup lang="ts">
 const { data } = await useAsyncData('navigation', () => queryCollectionNavigation('data', ['description', 'meta']))
 </script>
 
 <template>
   <div>
     <slot />
-    <Collection :data="data[0].children" />
+    <Collection :data="data.find(item => item.path === $route.path).children" />
   </div>
 </template>
 
