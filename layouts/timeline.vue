cccffa657f8896ab67e686117d11d6b34b97eb2d
satche <info@thomas-robert.ch>
2025-05-31 15:52:33 +0200

    feat: add a draft of the timeline component

---
+++ layouts/timeline.vue
@@ -0,0 +1,13 @@
+<script setup>
+const route = useRoute()
+const { data } = await useAsyncData('navigation', () => {
+    return queryCollectionNavigation('data', ['description', 'meta'])
+        .where("path", "LIKE", route.path + '%')
+})
+</script>
+
+<template>
+    <div>
+        <Timeline :data="data[0].children[0].children || []" />
+    </div>
+</template>
