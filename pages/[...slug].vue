6635ae6aefef2e8319a2e8f4ebfef6186ba7eba1
Satche <info@thomas-robert.com>
2025-11-21 20:30:54 +0000

    fix: return empty object if no content is found

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -13,7 +13,7 @@ definePageMeta({
 // Apply a layout based on the page's metadata
 const route = useRoute()
 const { data: page } = await useAsyncData(`${route.path}_page`, () => {
-  return queryCollection('data').path(route.path).first()
+  return queryCollection('data').path(route.path).first() || {}
 })
 
 const defaultCollectionLayouts = ["lore"]
