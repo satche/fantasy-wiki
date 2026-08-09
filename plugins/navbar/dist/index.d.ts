f76d0a99938fdb09d18ead2a9e672b7aa77ac18f
Satche <info@thomas-robert.com>
2026-08-09 23:07:34 +0200

    chore: remove navbar and call it from repo

--- plugins/navbar/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Navbar, Navbar as default } from './components/index.js';
-export { PageGenerator, PageMatcher, QuartzComponent, QuartzComponentConstructor, QuartzComponentProps, QuartzEmitterPlugin, QuartzFilterPlugin, QuartzPageTypePlugin, QuartzPageTypePluginInstance, QuartzTransformerPlugin, StringResource, VirtualPage } from '@quartz-community/types';
--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,4 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-// import * as ExternalPlugin from "./.quartz/plugins"
 
 const config = await loadQuartzConfig()
 export default config
