4bd6914886c86ce16564f30bcd954025eb2efdc8
Satche <info@thomas-robert.com>
2026-09-07 15:22:39 +0200

    feat: add alpha asc order in page folder

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,14 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
+import type { QuartzPluginData } from "./quartz/plugins/vfile"
+
+// Alphabetical order in folder page
+const byTitleAlphabetical = (f1: QuartzPluginData, f2: QuartzPluginData): number => {
+  const title1 = (f1.frontmatter?.title ?? "").toLowerCase()
+  const title2 = (f2.frontmatter?.title ?? "").toLowerCase()
+  return title1.localeCompare(title2, "fr", { numeric: true, sensitivity: "base" })
+}
+componentRegistry.setOptionOverrides("@quartz-community/folder-page", { sort: byTitleAlphabetical })
 
 const config = await loadQuartzConfig()
 export default config
